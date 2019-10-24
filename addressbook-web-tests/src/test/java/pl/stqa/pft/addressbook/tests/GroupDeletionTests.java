39a04005e9b7fe8f86efb97ad9e49e9f649922ba
ext-milena.drabik <ext-milena.drabik@finago.com>
2019-10-24 19:26:41 +0200

    added preconditions to group tests

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package pl.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import pl.stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase{
 
@@ -11,6 +12,9 @@ public class GroupDeletionTests extends TestBase{
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().goToGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -3,7 +3,7 @@ package pl.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.GroupData;
 
-public class GroupModificationTests extends TestBase{
+public class GroupModificationTests extends TestBase {
 
     public GroupModificationTests(String browser) {
         super(browser);
@@ -12,6 +12,9 @@ public class GroupModificationTests extends TestBase{
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().goToGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test4", "test2", null));
