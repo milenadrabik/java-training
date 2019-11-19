f3ec70da601c34280f57f345c547cd6c7fee4a59
ext-milena.drabik <ext-milena.drabik@finago.com>
2019-11-19 19:04:11 +0100

    added library for logs

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,21 @@
 package pl.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import pl.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TestBase extends ApplicationManager {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -23,4 +33,13 @@ public class TestBase extends ApplicationManager {
         app.stop();
     }
 
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
+    }
 }
